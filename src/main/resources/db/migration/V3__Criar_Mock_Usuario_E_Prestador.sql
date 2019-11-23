a7072c6a4d1553a719786e075fc9bdc16e2be552
Thayan Alves <thayan_fsa10@hotmail.com>
2019-11-22 22:17:59 -0200

    mock usuario sem prestador bugfixes

--- src/main/resources/db/migration/V3__Criar_Mock_Usuario_E_Prestador.sql
+++ src/main/resources/db/migration/V3__Criar_Mock_Usuario_E_Prestador.sql
@@ -2,6 +2,5 @@ INSERT INTO TB_USUARIO(id_usuario, email, senha, nome_completo, imagem) VALUES (
 INSERT INTO TB_ENDERECO(id_endereco,cep,cidade,uf,rua,bairro,numero,id_usuario) VALUES (1, 73803045, 'Formosa', 'GO', 'Avenida Circular', 'Abreu', 28, 1);
 INSERT INTO TB_PRESTADOR(id_prestador,telefone,rg,cnpj,cpf,id_usuario,id_servico) VALUES (1, 61996232697, 5909277, null, 06016692158,1,1);
 
-INSERT INTO TB_USUARIO(id_usuario, email, senha, nome_completo, imagem) VALUES (2, 'victor2@victor.com', '
-', 'Victor Victor Vitão', null);
+INSERT INTO TB_USUARIO(id_usuario, email, senha, nome_completo, imagem) VALUES (2, 'victor2@victor.com', '12345678', 'Victor Victor Vitão2', null);
 INSERT INTO TB_ENDERECO(id_endereco,cep,cidade,uf,rua,bairro,numero,id_usuario) VALUES (2, 73803045, 'Formosa', 'GO', 'Avenida Circular', 'Abreu', 28, 2);
